aca1dd2681d0bd21c65b0fe77c78d4d99db514b5
Befa79 <robindubois79@hotmail.com>
2021-02-05 10:16:46 +0000

    Your message here

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-    import  env
+    import env
 
 
 app = Flask(__name__)
@@ -14,15 +14,14 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo =  PyMongo(app)
+mongo = PyMongo(app)
 
- 
 
 @app.route("/")
-@app.route("/get_recipe")
-def get_recipe():
-    recipe = mongo.db.recipe.find()
-    return render_template("recipe.html", recipe=recipe)
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipe.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
