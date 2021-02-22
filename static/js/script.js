05d8df1bf069f1338134ba65be0ad400b399e913
Befa79 <robindubois79@hotmail.com>
2021-02-22 16:48:39 +0000

    msg

--- static/js/script.js
+++ static/js/script.js
@@ -3,5 +3,13 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $(".tooltipped").tooltip();
     $('select').formSelect();
-    $('.carousel').carousel();
 });
+
+function confirm_del(){
+  var delete_confirmed = confirm("Delete this sandwich recipe?")
+  if (delete_confirmed == true){
+      window.location.href = "url_for('delete_recipe', recipe_id=recipe._id)";
+  }
+}
+
+
